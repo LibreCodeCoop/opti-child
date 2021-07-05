ba18d2dfe61cbf0f9a43a419ef8538f37e692c32
LiviaGouvea <gouvea.lc@gmail.com>
2021-07-05 17:00:53 -0300

    Customiza header fiocruz

--- header-fiocruz.php
+++ header-fiocruz.php
@@ -1,9 +1,9 @@
 
 <div id="ensp-fiocruz">
-	<a href="http://ensp.fiocruz.br/" id="logoENSP">
+	<a href="http://ensp.fiocruz.br/"  target="_blank" id="logoENSP">
 		<img src="/wp-content/uploads/2021/07/logo_ensp2.png" />
 	</a>
-	<a href="http://www.ensp.fiocruz.br/" id="logoFiocruz">
+	<a href="http://www.ensp.fiocruz.br/" target="_blank" id="logoFiocruz">
 		<img src="/wp-content/uploads/2021/07/logo_fiocruz2.png"/>
 	</a>
 </div>
