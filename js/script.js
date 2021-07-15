c024df9c356154fff8caf9f7443c2e5831385922
LiviaGouvea <gouvea.lc@gmail.com>
2021-07-15 19:47:04 -0300

    Adiciona ações para customizar a página Docentes e comentários ao código

--- js/script.js
+++ js/script.js
@@ -1,32 +1,50 @@
+// Insere foto após nome do professor em página Docentes. 
+// Remove link de nome de professores
+const featuredImage = document.querySelectorAll('#post-989 .wp-block-latest-posts__featured-image');
+const arrowSVG = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26"><path d="M13,15.405l8.764-8.584c0.392-0.383,1.019-0.38,1.406,0.008l1.536,1.536c0.391,0.391,0.39,1.026-0.002,1.417L13.707,20.707 C13.512,20.902,13.256,21,13,21c-0.256,0-0.512-0.098-0.707-0.293L1.296,9.782c-0.393-0.39-0.394-1.025-0.002-1.417L2.83,6.829 c0.387-0.387,1.015-0.391,1.406-0.008L13,15.405z"/></svg>';
 
-const a = document.querySelectorAll('.wp-block-latest-posts__list:first-child li a');
-const li = document.querySelectorAll('.wp-block-latest-posts__list:nth-child(2) li');
+featuredImage.forEach((element) => {
+
+  var postTitle = element.nextSibling;
+
+  postTitle.removeAttribute("href");
+
+  postTitle.textContent += ' v';
+
+  postTitle.parentNode.insertBefore(postTitle, element);
+});
+
+
+// Adiciona ação em clique de lista de posts para exibir conteúdo dos respectivos posts na página Apresentação
+const a = document.querySelectorAll('#post-5 .wp-block-latest-posts__list:first-child li a');
+const li = document.querySelectorAll('#post-5 .wp-block-latest-posts__list:nth-child(2) > li');
 
 a.forEach((elementLi, index) => {  
 
   elementLi.removeAttribute("href");
-  elementLi.addEventListener("click", (evt, cityName) => {
+  elementLi.addEventListener("click", (evt) => {
 
     var i;
 
-    // Get all 'li' elements and hide them 
+    // Pega todos os elementos da lista e os esconde
     for (i = 0; i < li.length; i++) {
       li[i].style.display = "none";
     }
 
-    // Get all 'a' elements and remove the class "active"
+    // Altere a cor dos links para a cor padrão de link
     for (i = 0; i < a.length; i++) {
-        a[i].className = a[i].className.replace(" active", "");
+        a[i].className = a[i].style.color = '#237992'
     }
 
-    // Show the current tab, and add an "active" class to the element that opened the tab
+    // Exibe a guia atual e altera a cor do link que abriu a guia para a cor de link ativo
     li[index].style.display = "block";
-    evt.currentTarget.className += " active";
+    evt.currentTarget.style.color = '#f48634';
   });
 });
 
 
-const h3 = document.querySelectorAll('.wp-block-group__inner-container h3');
+// Cria dropdown para o conteúdo de listas na página Apresentação
+const h3 = document.querySelectorAll('#post-5 .wp-block-group__inner-container h3, #post-989 .wp-block-latest-posts__list li a');
 
 const togle = (el) => {
     el.style.display.length === 0
@@ -34,9 +52,6 @@ const togle = (el) => {
       : (el.style.display = "");
 };
 
-/**
- * Altera o elemento chamando o toggle para alterar entre exibir e esconder o elemento
- */
 h3.forEach((element) => {
   element.addEventListener("click", (event) => {
     for (let i = 0; i < element.parentNode.children.length; i++) {
